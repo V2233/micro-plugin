a41cdd4d44895543271191936d5d0a9650a3f88c
Small-v <2330660495@qq.com>
2024-11-22 15:25:32 +0800

    feat: 状态大屏增加自动刷新和全屏按钮

--- public/static/assets/index-7320b6d7.js
+++
@@ -1,1 +0,0 @@
-import{ad as t}from"./index-5f40d784.js";const e=s=>t.get("/bot/logs?id="+s),o=()=>t.get("/bot/status");export{e as a,o as r};
---
+++ public/static/assets/index-7bf5870e.js
@@ -0,0 +1,1 @@
+import{ac as t}from"./index-22075a7d.js";const e=s=>t.get("/bot/logs?id="+s),o=()=>t.get("/bot/status");export{e as a,o as r};
